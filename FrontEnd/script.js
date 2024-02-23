bc915f73e155e36db879e13587c9d338a3bfd802
AlexandreGauthe <155651944+AlexandreGauthe@users.noreply.github.com>
2024-02-23 15:07:09 +0100

    Dernières corrections

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,6 +2,40 @@
 export const gallery = document.querySelector(".gallery");
 export const reponse = await fetch("http://localhost:5678/api/works").then(reponse => reponse.json());
 
+//Fonction pour afficher les différents travaux de l'API dans la gallerie principale//
+export function afficherTravaux(reponse){
+   
+    //  Boucle for pour parcourir tout les travaux //
+        for (let i = 0; i < reponse.length; i++){
+    
+    //  Déclaration des variable pour traiter le travaux du tour de la boucle et créer les éléments HTML//        
+            const travaux = reponse[i];
+            const figure = document.createElement("figure");
+            const imageTravaux = document.createElement("img");
+            const titre = document.createElement("figcaption");
+    
+    //  Attribution des images et des titres aux variables précédemment déclarées      
+            imageTravaux.src = travaux.imageUrl;
+            titre.innerText = travaux.title;
+    
+    //  Placement des différents éléments ainsi récupérés dans les balises précédémment crées //        
+            figure.appendChild(imageTravaux);
+            figure.appendChild(titre);
+            gallery.appendChild(figure);
+        }    
+    }
+    
+// Appel de la fonction //
+afficherTravaux(reponse);
+
+// Fonction pour raffraichir la liste des travaux de l'API //
+export  async function actualiserTravaux(){
+    gallery.innerHTML="";
+    const actu = await fetch("http://localhost:5678/api/works").then(actu => actu.json());
+    afficherTravaux(actu);
+}
+    
+
 // Récupération des différentes catégories pour générer les boutons de filtres //
 const categories = reponse.map(reponse => reponse.category.name);
 const categoriesID= reponse.map(reponse => reponse.category.id);
@@ -51,42 +85,7 @@ for(let i=0; i< boutonsMenuFiltres.length; i++)
        
 })
 
-
-// Fonction pour raffraichir la liste des travaux de l'API //
-export  async function actualiserTravaux(){
-    gallery.innerHTML="";
-    const actu = await fetch("http://localhost:5678/api/works").then(actu => actu.json());
-    afficherTravaux(actu);
-}
-
-//Fonction pour afficher les différents travaux de l'API dans la gallerie principale//
-export function afficherTravaux(reponse){
-   
-//  Boucle for pour parcourir tout les travaux //
-    for (let i = 0; i < reponse.length; i++){
-
-//  Déclaration des variable pour traiter le travaux du tour de la boucle et créer les éléments HTML//        
-        const travaux = reponse[i];
-        const figure = document.createElement("figure");
-        const imageTravaux = document.createElement("img");
-        const titre = document.createElement("figcaption");
-
-//  Attribution des images et des titres aux variables précédemment déclarées      
-        imageTravaux.src = travaux.imageUrl;
-        titre.innerText = travaux.title;
-
-//  Placement des différents éléments ainsi récupérés dans les balises précédémment crées //        
-        figure.appendChild(imageTravaux);
-        figure.appendChild(titre);
-        gallery.appendChild(figure);
-    }    
-}
-
-// Appel de la fonction //
-afficherTravaux(reponse);
-
 // Fonction pour gérer le bouton qui affiche tout les travaux 
-
 function afficherToutTravaux(){
 
 // Récuperation du bouton //
@@ -101,7 +100,6 @@ function afficherToutTravaux(){
     })
 
 }
-
 // Appel de la fonction //
 afficherToutTravaux();
 
